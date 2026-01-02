df1f180d2a245d6b0d40b77700e650af7c770fee
Rohit Kumar Mahaur <141898177+RKMahaur@users.noreply.github.com>
2026-01-03 00:24:52 +0530

    Add files via upload

---
+++ Machine Learning/Student Performance Indicator/application.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+app = Flask(__name__)  # Entry point
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            math_score=float(request.form.get('math_score')),
+            reading_score=float(request.form.get('reading_score'))
+        )
+        
+        pred_df = data.get_data_as_data_frame()
+
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+
+        predicted_writing_score = round((predict_pipeline.predict(pred_df)[0]),2)
+        print("After Prediction")
+        
+        # Calculate the average score
+        average_score = round((data.math_score + data.reading_score + predicted_writing_score) / 3, 2)
+        
+        return render_template('home.html', predicted_writing_score=predicted_writing_score, average_score=average_score)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
---
+++ Machine Learning/Student Performance Indicator/src/components/data_ingestion.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.data_ingestion_config.raw_data_path,index=False,header=True)   # Saving raw data in csv
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)   # Saving train data in csv 
+            test_set.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)   # Saving test data in csv 
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.data_ingestion_config.train_data_path,     # returning for Data Transformation
+                self.data_ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print("The best model accuracy is : ",modeltrainer.initiate_model_trainer(train_arr,test_arr))
